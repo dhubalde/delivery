c6610c3b569aa67ab03c22017c31f193163adc77
dhubalde <dalilahubalde@hotmail.com>
2026-08-30 19:18:22 -0300

    fix(business-date): change cutoff to 03:00

--- backend/apps/common/utils.py
+++ backend/apps/common/utils.py
@@ -5,5 +5,5 @@ from django.utils import timezone
 
 def get_business_date():
     now = timezone.localtime(timezone.now())
-    cutoff = now.replace(hour=5, minute=0, second=0, microsecond=0)
+    cutoff = now.replace(hour=3, minute=0, second=0, microsecond=0)
     return cutoff.date() if now >= cutoff else (cutoff - timedelta(days=1)).date()
